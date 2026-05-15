532b453577871b4b4bc5bfad1d1d36a45d37af4a
Nidhish Chauhan <solemnsquire@gmail.com>
2026-05-15 23:53:14 +0530

    bashmaze: init at unstable

---
+++ pkgs/by-name/ba/bashmaze/package.nix
@@ -0,0 +1,37 @@
+{
+    lib,
+    stdenvNoCC,
+    fetchFromGithub,
+}:
+
+
+stdenvNoCC.mkDerivation(finalAttrs:{
+
+    pName = "bashmaze";
+    version = "unstable-2014-05-02";
+
+    src = fetchFromGithub {
+        owner = "phoemur";
+        repo = "https://github.com/phoemur/bashmaze";
+        rev = "eaa8007f6e9b6167e54ec8f64dc70572e88c15f2";
+        hash = lib.fakeHash;
+    };
+
+    dontBuild = true;
+
+    installPhase = ''
+        runHook preInstall
+
+        install -Dm755 bashmaze.sh $out/bin/bashmaze
+
+        runHook postInstall
+    '';
+
+    meta = {
+        description = "Maze game written completely in bash-script.";
+        homepage = "https://github.com/phoemur/bashmaze";
+        license = lib.licenses.no
+        platforms = lib.platforms.linux;
+        maintainers = lib.maintainers; [castorNova2];
+    }
+})
