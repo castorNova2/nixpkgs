0d13fff0993636429f89a0505323e791cb0fc6b0
Nidhish Chauhan <solemnsquire@gmail.com>
2026-05-19 00:15:46 +0530

    zztgo: init at 0-unstable-2020-05-29

---
+++ pkgs/by-name/zz/zztgo/package.nix
@@ -0,0 +1,39 @@
+{
+  lib,
+  fetchFromGitHub,
+  buildGoModule,
+  makeWrapper,
+}:
+
+buildGoModule {
+
+  pname = "zztgo";
+  version = "0-unstable-2020-05-29";
+
+  src = fetchFromGitHub {
+    owner = "benhoyt";
+    repo = "zztgo";
+    rev = "9edb1452d887852c5c68cae0a91a6227cd4ef7a9";
+    hash = "sha256-Wz9xAcsT27scuR78X6+17l0RExpmh0uTQUOcQ9lHIkI=";
+  };
+
+  vendorHash = "sha256-0hOXo7Ww34yI5yrz4CDMuFZjPj9CqtmWxQoc9aEBFOs=";
+
+  nativeBuildInputs = [ makeWrapper ];
+  postInstall = ''
+    install -Dm644 TOWN.ZZT $out/share/zztgo/TOWN.ZZT
+    install -Dm644 zzt.terminal $out/share/zztgo/zzt.terminal
+
+    wrapProgram $out/bin/zztgo \
+      --chdir $out/share/zztgo
+  '';
+
+  meta = with lib; {
+    mainProgram = "zztgo";
+    description = "Port of ZZT to Go";
+    homepage = "https://github.com/benhoyt/zztgo";
+    license = licenses.mit;
+    platforms = platforms.unix;
+    maintainers = with maintainers; [ castorNova2 ];
+  };
+}
